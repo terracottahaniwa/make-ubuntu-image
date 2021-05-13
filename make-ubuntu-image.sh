7a1ac5aacf15e9709a2b573c17688c46720c7fd8
terracottahaniwa <terracottahaniwa@gmail.com>
2021-05-13 23:43:47 +0000

    update

--- make-ubuntu-image.sh
+++ make-ubuntu-image.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-umount mnt/root/efi
-umount mnt/dev
-umount mnt/sys
-umount mnt/proc
-umount -l mnt
-losetup -D
-rm -f img
-rm -rf mnt
+if [ -d mnt ]; then
+  umount mnt/root/efi
+  umount mnt/dev
+  umount mnt/sys
+  umount mnt/proc
+  umount -l mnt
+  rm -rf mnt
+  losetup -D
+fi
 
-fallocate -l 4G img
+if [ -f img ]; then
+  rm -f img
+fi
+
+fallocate -l 6G img
 parted -s -a opt img mklabel gpt
 parted -s -a opt img mkpart fat32  0% 512M
 parted -s -a opt img mkpart ext4 512M 100%
@@ -45,21 +50,21 @@ UUID2=$(blkid -s UUID $PART2 | awk '{ print $2 }')
 
 echo "$UUID1\t/boot/efi\tvfat\tumask=0077\t0\t0" >> mnt/etc/fstab
 echo "$UUID2\t/\text4\terrors=remount-ro\t0\t1"  >> mnt/etc/fstab
+echo "tmpfs\t/tmp\ttmpfs\tdefaults\t0\t0"        >> mnt/etc/fstab
 
-chroot mnt sh -c "export DEBIAN_FRONTEND=noninteractive && apt update && apt upgrade -y && apt clean"
-chroot mnt sh -c "export DEBIAN_FRONTEND=noninteractive && apt install linux-image-generic grub-efi bash-completion ufw ssh avahi-daemon -y"
+chroot mnt sh -c "export DEBIAN_FRONTEND=noninteractive && apt update && apt upgrade -y"
+chroot mnt sh -c "export DEBIAN_FRONTEND=noninteractive && apt install linux-image-generic shim-signed grub-efi-amd64-signed mokutil -y"
+chroot mnt sh -c "export DEBIAN_FRONTEND=noninteractive && apt install network-manager ufw avahi-daemon ssh bash-completion ubuntu-desktop -y"
+chroot mnt sh -c "export DEBIAN_FRONTEND=noninteractive && apt clean"
 
 chroot mnt sh -c "grub-install"
-chroot mnt sh -c "sed -i.old -e 's/gfxpayload_dynamic=\"1\"/gfxpayload_dynamic=\"0\"/g'                              /etc/grub.d/10_linux"
-chroot mnt sh -c "sed -i     -e '/^vt_handoff=\"1\"$/a GRUB_CMDLINE_LINUX=\"consoleblank=60 systemd.show_status=1\"' /etc/grub.d/10_linux"
+chroot mnt sh -c "sed -i -e 's/^gfxpayload_dynamic=\"1\"$/gfxpayload_dynamic=\"0\"/g' /etc/grub.d/10_linux"
+chroot mnt sh -c "sed -i -e '/^GRUB_CMDLINE_LINUX=/s/\"\"/\"systemd.show_status=1 modprobe.blacklist=nouveau\"/g' /etc/default/grub"
 chroot mnt update-grub
 
-chroot mnt sh -c "echo 'network:'             >> /etc/netplan/networkd.yaml"
-chroot mnt sh -c "echo '  version: 2'         >> /etc/netplan/networkd.yaml"
-chroot mnt sh -c "echo '  renderer: networkd' >> /etc/netplan/networkd.yaml"
-chroot mnt sh -c "echo '  ethernets:'         >> /etc/netplan/networkd.yaml"
-chroot mnt sh -c "echo '    enp3s0:'          >> /etc/netplan/networkd.yaml"
-chroot mnt sh -c "echo '      dhcp4: true'    >> /etc/netplan/networkd.yaml"
+chroot mnt sh -c "echo 'network:'                   >> /etc/netplan/NetworkManager.yaml"
+chroot mnt sh -c "echo '  version: 2'               >> /etc/netplan/NetworkManager.yaml"
+chroot mnt sh -c "echo '  renderer: NetworkManager' >> /etc/netplan/NetworkManager.yaml"
 chroot mnt sh -c "echo 'hirsute' > /etc/hostname"
 chroot mnt sh -c "hostname $(echo /etc/hostname)"
 
